7752d14f81c9f1c5f2e509aea3c15ac2abaecd8d
Daniel Dvorkin <danieldvorkin@Daniels-MacBook-Pro.local>
2018-04-08 10:57:47 -0400

    Added a theme

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
+  get 'contact', to: 'home#contact', as: 'contact'
+  resources :projects
 end
